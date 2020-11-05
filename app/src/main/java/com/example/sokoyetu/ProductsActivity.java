56010bf52d785e5a0e1fca84b5b4f1642793febc
Juliet <julielgisemba000@gmail.com>
2020-11-05 06:48:30 +0300

    modified the files

--- app/src/main/java/com/example/sokoyetu/ProductsActivity.java
+++ app/src/main/java/com/example/sokoyetu/ProductsActivity.java
@@ -1,4 +1,13 @@
 package com.example.sokoyetu;
 
-public class ProductsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
 }
