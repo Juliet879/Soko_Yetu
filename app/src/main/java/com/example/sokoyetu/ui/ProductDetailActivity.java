5e7637ca01aac4dc952efeb238050bcbaa16d4d7
Juliet <julielgisemba000@gmail.com>
2020-11-06 06:26:58 +0300

    modified the files

--- app/src/main/java/com/example/sokoyetu/ui/ProductDetailActivity.java
+++ app/src/main/java/com/example/sokoyetu/ui/ProductDetailActivity.java
@@ -1,4 +1,40 @@
 package com.example.sokoyetu.ui;
 
-public class ProductDetailActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.sokoyetu.R;
+import com.example.sokoyetu.adapters.ProductPagerAdapter;
+import com.example.sokoyetu.models.Datum;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private ProductPagerAdapter adapterViewPager;
+    List<Datum> mData_product = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+
+        ButterKnife.bind(this);
+
+        mData_product = Parcels.unwrap(getIntent().getParcelableExtra("data_product"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new ProductPagerAdapter(getSupportFragmentManager(), mRestaurants);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+    }
 }
---
+++ app/src/main/java/com/example/sokoyetu/ui/ProductDetailFragment.java
@@ -0,0 +1,4 @@
+package com.example.sokoyetu.ui;
+
+public class ProductDetailFragment {
+}
